c0b24dc4bfb595149e797c330b6a93c03d5be7a6
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-07-26 22:11:09 +0500

    javascript

--- script.js
+++ script.js
@@ -1,72 +1,105 @@
-// function Sinup() {
-//    var arrays = JSON.parse(localStorage.getItem("name")) || [];
-//    let email = document.getElementById("email").value;
-//    let password = document.getElementById("pass").value;
-//    let users = {
-//       email,
-//       password
-//    }
+var arrays = JSON.parse(localStorage.getItem("name")) || [];
+function Sinup() {
+  let fName=document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+  let users = {
+    email,
+    password
+  }
 
-//    localStorage.setItem("name", JSON.stringify(arrays));
-//    localStorage.getItem("name")
-//    //   localStorage.clear(name);
-//    // isFound=false;    
-//      for(let i=0;i<arrays.length;i++){
-//       if(email === arrays[i].email){
-//          console.log("User Exsit");
-//       }
-//    }
+  localStorage.setItem("name", JSON.stringify(arrays));
+  //  localStorage.getItem("name")
+  //   localStorage.clear(name);
+  let isFound = true;
+  for (let i = 0; i < arrays.length; i++) {
+    if (email === arrays[i].email) {
+      alert("User Exsit");
+      isFound = false;
+    }
+    
+  }
+  if(isFound === true){
+    arrays.push(users);
+    console.log(arrays);
+    window.open("page.html");
+  }
+  
+  
+  let uName=document.getElementById("userName").innerText = fName.value;
+  console.log(uName)
+  console.log(fName);
+  //  console.log(arrays.length)
 
+}
 
-//    console.log(arrays.length)
-//    arrays.push(users);
-
-//    console.log(arrays);
-// }
 //  console.log( typeof arrays);
 
+// loginSection
+
+function loginfnc() {
+  let LEmail = document.getElementById("LEmail").value;
+  // let Lpassword=document.getElementById("Lpassword").value;
+isFound =true;
+  for (let i = 0; i < arrays.length; i++) {
+    if (LEmail == arrays[i].email) {
+      window.open("page.html");
+      isFound=false;
+    }
+  }
+  if(isFound=true){
+    alert("pleace sinup ")
+  }
+}
 
 
 
 
 // blog Section 
- 
-let blogArray=JSON.parse(localStorage.getItem("bl")) || [];
-function bloges(){
-   let add={
-     blogTittle : document.getElementById('blogTittle').value,
-     blog : document.getElementById('blog').value,
-     authorName : document.getElementById('authorName').value
-     }
-     localStorage.setItem("bl",JSON.stringify(add));
-     blogArray.push(add);
-     console.log(blogArray);
-    //  document.write(blogArray);
 
-    //  let panel=document.getElementById("panel");
-    //  panel.style.display ="flex";
-   // alert("hello")
+let blogArray = JSON.parse(localStorage.getItem("bl")) || [];
+function bloges() {
+  let blogTittle = document.getElementById('blogTittle').value;
+  let blog = document.getElementById('blog').value;
+  let authorName = document.getElementById('authorName').value;
+let fileIMG=document.getElementById("file").value;
+  let add = {
+    blogTittle,
+    blog,
+    authorName,
+    fileIMG
 
-   // console.log(blogTittle,blog,authorName);
-   // document.write(blogTittle,blog,authorName +"<br>");
-   
-   // document.write(blogTittle,blog,authorName);
+  }
+  localStorage.setItem("bl", JSON.stringify(blogArray));
+  blogArray.push(add);
+   console.log(blogArray);
+  //  document.write(blogArray);
+
+  console.log(add.blogTittle)
+
+
+  document.write(blogTittle, blog, authorName);
 }
-function bloges(){
-   let panel=document.getElementById("panel");
-     panel.style.display ="flex";
-     form.style.display ="none";
-    //  document.write(blogArray[0].add);
-     
+function bloges() {
+  let panel = document.getElementById("panel");
+  panel.style.display = "flex";
+  form.style.display = "none";
+  //  document.write(blogArray[0].add);
+  let name = document.getElementById('name').innerText = blogTittle.value;
+  let description = document.getElementById('decription').innerText = blog.value;
+  let author = document.getElementById("author").innerText = authorName.value;
+  let file=document.getElementById("fImage").innerText=fileIMG.value;
+  //  console.log(blog);     
 
 }
-console.log("hello")
+// console.log("hello")
 
 
 
-function panel(){
-  let form=document.getElementById("form");
-  form.style.display ="flex";
-  document.getElementById("clickbtn");
+function panel() {
+  let form = document.getElementById("form");
+  form.style.display = "flex";
   // clickbtn.style.display="none"
 }
+
+
