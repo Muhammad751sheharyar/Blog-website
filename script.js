c0fe7fa3b8a96f59da11c8157510c1655e46b36d
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-08-17 23:58:45 +0500

    add java script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var arrays = JSON.parse(localStorage.getItem("name")) || [];
 function Sinup() {
-  let fName=document.getElementById("name").value;
+  
   let email = document.getElementById("email").value;
   let password = document.getElementById("pass").value;
   let users = {
@@ -10,35 +10,89 @@ function Sinup() {
 
   window.localStorage.setItem("name", JSON.stringify(arrays));
   //  localStorage.getItem("name")
-    // localStorage.clear("name");
-    let isFound = true;
-    for (let i = 0; i < arrays.length; i++) {
-        if (email === arrays[i].email) {
-            alert("User Exsit");
-            isFound = false;
-          }
-        
-        }
-        if(isFound === true){
-          arrays.push(users);
-      console.log(arrays);
-      window.open("page2.html");
+  // localStorage.clear("name");
+  let isFound = true;
+  for (let i = 0; i < arrays.length; i++) {
+    if (email === arrays[i].email) {
+      alert("User Exsit");
+      isFound = false;
     }
-    
+
+  }
+  if (isFound === true) {
+    arrays.push(users);
+    console.log(arrays);
+    window.open("page1.html");
+  }
 }
 
 function loginfnc() {
   let LEmail = document.getElementById("LEmail").value;
   // let Lpassword=document.getElementById("Lpassword").value;
-isFound =true;
+  isFound = true;
   for (let i = 0; i < arrays.length; i++) {
     if (LEmail == arrays[i].email) {
-      window.open("page2.html");
-      isFound=false;
+      window.open("page1.html");
+      isFound = false;
     }
   }
-  if(isFound=true){
+  if (isFound = true) {
     alert("pleace sinup ")
   }
 }
 
+
+
+function bloges() {
+
+
+  let blognameinp = document.getElementById('blogTittle').value;
+  let blogConentinp = document.getElementById('blogContent').value;
+  let authorNameinp = document.getElementById('authorName').value;
+  form.style.display = "none";
+      
+  // Dom Section
+
+  let mainPage = document.querySelector('.mainPage');
+  // console.log(mainPage);
+
+
+  // div 
+
+  let div = document.createElement('div');
+  div.innerText=blognameinp;
+  div.setAttribute('class','blogTittle');
+  mainPage.append(div);
+
+
+
+  let blogdesc=document.createElement("div");
+ blogdesc.innerText=blogConentinp;
+blogdesc.setAttribute('class','decription');
+// console.log(blogdesc);
+mainPage.append(blogdesc);
+console.log(blogdesc)
+
+
+
+let other=document.createElement("div");
+other.innerText=authorNameinp;
+other.setAttribute('class','autherName');
+mainPage.append(other);
+console.log(other);
+
+}
+
+
+
+function panel() {
+  let form = document.getElementById("form");
+  form.style.display = "flex";
+  // clickbtn.style.display="none"
+}
+
+
+function blLogout() {
+  // window.Location.href="login.html";
+  window.open("login.html")
+}
